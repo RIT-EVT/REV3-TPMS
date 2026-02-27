822605f45ba0676dfb5d6d312f14234ff50dd7a7
imnotRESIN <201021492+imnotRESIN@users.noreply.github.com>
2026-02-26 20:46:54 -0500

    Initial commit

---
+++ targets/BIKE_NAME-BOARD_NAME/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * This is a basic sample of using the UART module. The program provides a
+ * basic echo functionality where the uart will write back whatever the user
+ * enters.
+ */
+
+#include <core/io/UART.hpp>
+#include <core/io/pin.hpp>
+#include <core/manager.hpp>
+
+namespace io = core::io;
+
+int main() {
+    // Initialize system
+    core::platform::init();
+
+    // Setup UART
+    io::UART& uart = io::getUART<io::Pin::UART_TX, io::Pin::UART_RX>(9600);
+
+    // String to store user input
+    char buf[100];
+
+    while (1) {
+        // Read user input
+        uart.printf("Enter message: ");
+        uart.gets(buf, 100);
+        uart.printf("\n\recho: %s\n\r", buf);
+    }
+}
